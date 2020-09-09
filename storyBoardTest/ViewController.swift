f08839000200ef0c168dc2e5a99782864c044d89
Alex <alex.sallberg@gmail.com>
2020-09-09 23:59:39 +0200

    Testing some stuff out

--- storyBoardTest/ViewController.swift
+++ storyBoardTest/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var mainSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if(mainSwitch.isOn){
+            label.text = "Dark Mode"
+            label.textColor = .white
+            self.view.backgroundColor = .black
+        } else{
+            label.text = "Light mode"
+            label.textColor = .black
+            self.view.backgroundColor = .white
+        }
     }
 
-
+    @IBAction func flipSwitch(_ sender: Any) {
+        if(mainSwitch.isOn){
+            label.text = "Dark Mode"
+            label.textColor = .white
+            self.view.backgroundColor = .black
+        } else{
+            label.text = "Light mode"
+            label.textColor = .black
+            self.view.backgroundColor = .white
+        }
+      
+    }
+    
+    
+    
 }
-
